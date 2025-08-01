92dcdc87bda9cf3761f2575e917dff5b327b6b17
Mario Peña <mario.pena@bemobi.com>
2025-08-01 18:01:08 -0400

    Add docker compose health check. Change email-api dockerfile execution. Add delay between email sendings

--- email-sender/src/main/java/xyz/mnpc/Main.java
+++ email-sender/src/main/java/xyz/mnpc/Main.java
@@ -2,6 +2,7 @@ package xyz.mnpc;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import xyz.mnpc.db.DBAccess;
@@ -22,35 +23,45 @@ public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
     private static final HttpClient client = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(10)).build();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         DataSource dataSource = PostgresConfig.getDataSource();
-        JsonArray arr = DBAccess.getCuotas(dataSource, "Cecilia");
+        JsonArray arr = DBAccess.getCuotas(dataSource);
         for (JsonElement element : arr) {
             try {
-                sendMail(element.getAsJsonObject().toString());
+                sendMail(element.getAsJsonObject());
             } catch (Exception e) {
                 log.error("Sending Mail Exception ", e);
+                e.printStackTrace();
             }
-
+            Thread.sleep(1_000);
         }
     }
 
-    private static void sendMail(String body) throws IOException, InterruptedException {
+    private static void sendMail(JsonObject bodyObj) throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("http://127.0.0.1:8080/api/send"))
                 .version(HttpClient.Version.HTTP_1_1)
                 .timeout(Duration.ofSeconds(10))
                 .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(body, StandardCharsets.UTF_8))
+                .POST(HttpRequest.BodyPublishers.ofString(bodyObj.toString(), StandardCharsets.UTF_8))
                 .build();
         Instant start = Instant.now();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         Instant end = Instant.now();
         if (response.statusCode() != 200) {
-            log.error("Error sending mail: {} [et: {}ms]", response.body(), Duration.between(start, end).toMillis());
+            log.error("Error sending mail: [Apoderado: {}, correo: {}] [RES: {}] [et: {}ms]",
+                    bodyObj.get("parentName").getAsString(),
+                    bodyObj.get("to").getAsString(),
+                    response.body(),
+                    Duration.between(start, end).toMillis());
             return;
         }
-        log.info("Mail sent successfully: {} [et: {}ms]", response.body(), Duration.between(start, end).toMillis());
+        log.info("[{}|{}] Mail sent successfully: {} [et: {}ms]",
+                bodyObj.get("parentName").getAsString(),
+                bodyObj.get("to").getAsString(),
+                response.body(),
+                Duration.between(start, end).toMillis());
+
     }
 
 
